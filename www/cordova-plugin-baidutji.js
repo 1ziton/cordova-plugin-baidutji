40952b797a07673e41567484719ddd925b0ef760
liyeios <30946963+liyeios@users.noreply.github.com>
2017-12-04 16:59:27 +0800

    Update cordova-plugin-baidutji.js
    添加接口

--- www/cordova-plugin-baidutji.js
+++ www/cordova-plugin-baidutji.js
@@ -1,5 +1,50 @@
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'cordova-plugin-baidutji', 'coolMethod', [arg0]);
-};
+
+var errorHandle = function(error) {
+	console.log(error);
+}
+
+
+module.exports = {
+    //print something to the native console
+    startWithAppId: function(eventId, eventLabel) {
+        exec(null, errorHandle, "BaiduMobStat", "start", [appKey]);
+    },
+
+    onEvent: function(eventId, eventLabel) {
+        exec(null, errorHandle, "BaiduMobStat", "onEvent", [eventId, eventLabel]);
+    },
+
+    onEventWithAttributes: function(eventId, eventLabel, attributes) {
+        exec(null, errorHandle, "BaiduMobStat", "onEventWithAttributes", [eventId, eventLabel, attributes]);
+    },
+
+    onEventDuration: function(eventId, eventLabel, duration) {
+        exec(null, errorHandle, "BaiduMobStat", "onEventDuration", [eventId, eventLabel, duration]);
+    },
+
+    onEventDurationWithAttributes: function(eventId, eventLabel, duration, attributes) {
+        exec(null, errorHandle, "BaiduMobStat", "onEventDurationWithAttributes", [eventId, eventLabel, duration, attributes]);
+    },
+
+    onEventStart: function(eventId, eventLabel) {
+        exec(null, errorHandle, "BaiduMobStat", "onEventStart", [eventId, eventLabel]);
+    },
+
+    onEventEnd: function(eventId, eventLabel) {
+        exec(null, errorHandle, "BaiduMobStat", "onEventEnd", [eventId, eventLabel]);
+    },
+
+    onEventEndWithAttributes: function(eventId, eventLabel, attributes) {
+        exec(null, errorHandle, "BaiduMobStat", "onEventEndWithAttributes", [eventId, eventLabel, attributes]);
+    },
+
+    onPageStart: function(page) {
+        exec(null, errorHandle, "BaiduMobStat", "onPageStart", [page]);
+    },
+
+    onPageEnd: function(page) {
+        exec(null, errorHandle, "BaiduMobStat", "onPageEnd", [page]);
+    }
+}
